a3e9a7d4af32dfab6739ab6c504181e5b05abca5
George Ikigu <ikigu.geo@gmail.com>
2024-08-28 17:15:12 +0300

    solution: task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,2 @@
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, YEAR(CURDATE()) - 4) - formed) AS lifespan FROM metal_bands WHERE FIND_IN_SET('Glam rock', style) > 0 ORDER BY lifespan DESC;
